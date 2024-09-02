b070af3f3178cb94196efc4304ddc0b0ed3c585c
Chris Hughes <chris@chris-hughes.dev>
2024-09-02 10:23:40 -0300

    Functional wall drawing

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
+#define GRID_HEIGHT 3
+#define GRID_WIDTH 5
+
 
 typedef struct {
   int y;
@@ -13,8 +16,35 @@ typedef enum  {
   ADD,
   GRID,
   SELECT,
+  WALL,
   EXIT
 } Menu_state;
+
+typedef enum {
+  TL,
+  T,
+  TR,
+  L,
+  M,
+  R,
+  BR,
+  B,
+  BL,
+  E
+}Grid_pos;
+
+
+typedef struct {
+  Grid_pos grid_pos;
+  bool is_wall;
+}Grid_state;
+
+typedef struct {
+  Grid_state left;
+  Grid_state right;
+  Grid_state up;
+  Grid_state down;
+}Proximal_states;
 
 typedef struct {
   char *code;
@@ -172,9 +202,13 @@ Menu_state net_menu(Position *pos, WINDOW *win, Netcode netcodes[], size_t menu_
         mvwprintw(win, i + 5, 2, "%s %s", netcodes[i].code, netcodes[i].label);
       }
       wrefresh(win);
-      print_char(pos, netcodes[highlight].code,
-               pos->curr_row, pos->curr_col);
-      return (Menu_state) GRID;
+      if (highlight == 0) {
+        return (Menu_state) WALL;
+      } else {
+        print_char(pos, netcodes[highlight].code,
+                   pos->curr_row, pos->curr_col);
+        return (Menu_state) GRID;
+      }
       break;
     case 'q':
       for (size_t i = 0; i < menu_len ; i++) {
@@ -230,16 +264,146 @@ Menu_state grid_movement(Position *pos) {
   }
 }
 
+Proximal_states get_proximal_states(Position *pos, Grid_state *grid_state) {
+  Grid_state left;
+  Grid_state right;
+  Grid_state up;
+  Grid_state down;
+
+  if (pos->curr_col > 1) {
+    left = grid_state[(pos->curr_row - 1) * pos->width + (pos->curr_col - 2)];
+  } else {
+    left = (Grid_state){E, false};
+  }
+
+  if (pos->curr_col < (int)pos->width) {
+    right = grid_state[(pos->curr_row - 1) * pos->width + (pos->curr_col)];
+  } else {
+    right = (Grid_state){E, false};
+  }
+
+  if (pos->curr_row > 1) {
+    up = grid_state[(pos->curr_row - 2) * pos->width + (pos->curr_col - 1)];
+  } else {
+    up = (Grid_state){E, false};
+  }
+
+  if (pos->curr_row < (int)pos->height) {
+    down = grid_state[(pos->curr_row) * pos->width + (pos->curr_col - 1)];
+  } else {
+    down = (Grid_state){E, false};
+  }
+
+  return (Proximal_states) {
+    left, right, up, down};
+}
+
+void create_wall(Position *pos, Proximal_states p_states) {
+    for (int col = 1; col < GRID_WIDTH - 1; col++) {
+      if (p_states.up.is_wall == true) {
+        mvwaddstr(get_pos(pos, pos->curr_row,
+                          pos->curr_col), 0, col, "\u2588");
+      } else {
+        mvwaddstr(get_pos(pos, pos->curr_row,
+                          pos->curr_col), 0, col, "\u2584");
+      }
+
+      if (p_states.down.is_wall == true) {
+        mvwaddstr(get_pos(pos, pos->curr_row,
+                          pos->curr_col), 2, col, "\u2588");
+      } else {
+        mvwaddstr(get_pos(pos, pos->curr_row,
+                          pos->curr_col), 2, col, "\u2580");
+      }
+    }
+
+    if (p_states.left.is_wall == true) {
+      mvwaddstr(get_pos(pos, pos->curr_row,
+                        pos->curr_col), 1, 0, "\u2588");
+    } else {
+      mvwaddstr(get_pos(pos, pos->curr_row,
+                        pos->curr_col), 1, 0, "\u2590");
+    }
+
+    if (p_states.right.is_wall == true) {
+      mvwaddstr(get_pos(pos, pos->curr_row,
+                        pos->curr_col), 1, 4, "\u2588");
+    } else {
+      mvwaddstr(get_pos(pos, pos->curr_row,
+                        pos->curr_col), 1, 4, "\u258C");
+    }
+
+    for (int col = 1; col < GRID_WIDTH - 1; col++) {
+      mvwaddstr(get_pos(pos, pos->curr_row,
+                        pos->curr_col), 1, col, "\u2588");
+    }
+
+
+    move_cur(pos, pos->curr_row, pos->curr_col);
+    wrefresh(get_pos(pos, pos->curr_row, pos->curr_col));
+  
+}
+
+Menu_state create_wall_mode(Position *pos, Grid_state *grid_state) {
+  int choice = '\n';
+  while (1) {
+  Proximal_states p_states = get_proximal_states(pos, grid_state);
+
+    switch (choice) {
+    case KEY_UP:
+      if (pos->curr_row > 1) {
+        pos = move_cur(pos, pos->curr_row - 1,
+                            pos->curr_col);
+      }
+      break;
+    case KEY_LEFT:
+      if (pos->curr_col > 1) {
+        pos= move_cur(pos, pos->curr_row,
+                            pos->curr_col - 1);
+      }
+      break;
+    case KEY_DOWN:
+      if (pos->curr_row < (int)pos->height) {
+        pos= move_cur(pos, pos->curr_row + 1,
+                            pos->curr_col);
+      }
+      break;
+    case KEY_RIGHT:
+      if (pos->curr_col < (int)pos->width) {
+        pos= move_cur(pos, pos->curr_row,
+                            pos->curr_col + 1);
+      }
+      break;
+    case '\n':
+      /* move_cur(pos, pos->curr_row, pos->curr_col); */
+      /* return (Menu_state) SELECT; */
+      grid_state[(pos->curr_row - 1) * pos->width + (pos->curr_col - 1)].is_wall = true;
+      create_wall(pos, p_states);
+      break;
+    case 'q':
+      return (Menu_state) SELECT;
+    };
+    choice = getch();
+  }
+  return (Menu_state) GRID;
+}
+
+
 int main() {
   setlocale(LC_ALL, "");
   initscr();
   noecho();
   start_color();
+  init_pair(1, COLOR_MAGENTA, COLOR_MAGENTA);
   size_t width = 30;
   size_t height = 20;
+  Grid_state *grid_state=
+    (Grid_state *)malloc(sizeof(Grid_state) * height * width);
+
+
   Position *position =
     (Position *)malloc((sizeof(int) * 2) + (sizeof(size_t) * 2) +
-                         (sizeof(WINDOW*) * height * width + 1));
+                         (sizeof(WINDOW*) * height * width));
   position->height = height;
   position->width = width;
   position->curr_col = 1;
@@ -260,43 +424,54 @@ int main() {
   center_horizontal(menu, "-----------", 3);
 
   Netcode netcodes[21] = {
-    {"\u20ac", "foo"},
-    {"\u0040", "bar"},
-    {"\u00a5", "baz"}};
+    {"\u2588", "Wall"},
+    {"=", "Gate 2"},
+    {"\u2261", "Gate 3"},
+    {"\u2263", "Gate 4"}};
 
-  size_t net_menu_len = 3;
+  size_t net_menu_len = 4;
 
   for (size_t i = 0; i < net_menu_len; i++) {
     mvwprintw(menu, i + 5, 2, "%s %s", netcodes[i].code, netcodes[i].label);
   }
 
   wrefresh(menu);
-  curs_set(2);
+
+  refresh();
 
   size_t i = 0;
   for (size_t row = first_row; row <= last_row; row += 2) {
     for (size_t col = first_col; col <= last_col; col += 4) {
       refresh();
-      WINDOW *win = newwin(3, 5, row, col);
+      WINDOW *win = newwin(GRID_HEIGHT, GRID_WIDTH, row, col);
       if (row == first_row && col == first_col) {
         top_left_box(win);
+        grid_state[i] = (Grid_state){TL, false};
       } else if (row == first_row && col != last_col) {
         top_box(win);
+        grid_state[i] = (Grid_state){T, false};
       } else if (row == first_row && col == last_col) {
         top_right_box(win);
+        grid_state[i] = (Grid_state){TR, false};
       } else if (row != first_row && col == first_col && row != last_row) {
         left_box(win);
+        grid_state[i] = (Grid_state){L, false};
       } else if (row != first_row && col != first_col &&
                  row != last_row && col != last_col) {
         mid_box(win);
+        grid_state[i] = (Grid_state){M, false};
       } else if (row != first_row && row != last_row && col == last_col) {
         right_box(win);
+        grid_state[i] = (Grid_state){R, false};
       } else if (row == last_row && col == first_col) {
         bottom_left_box(win);
+        grid_state[i] = (Grid_state){BL, false};
       } else if (row == last_row && col != last_col) {
         bottom_box(win);
+        grid_state[i] = (Grid_state){B, false};
       } else if (row == last_row && col == last_col) {
         bottom_right_box(win);
+        grid_state[i] = (Grid_state){BL, false};
       }
       position->grid[i] = win;
       i++;
@@ -323,6 +498,9 @@ int main() {
       free(position);
       endwin();
       return 0;
+    case WALL:
+      menu_state = create_wall_mode(position, grid_state);
+      break;
     }
   }
 
@@ -331,3 +509,47 @@ int main() {
 
   return 0;
 }
+
+
+  /* for (int col = 1; col < GRID_WIDTH - 1; col++) { */
+  /*   if (p_states.up.is_wall == true) { */
+  /*     mvwaddstr(get_pos(pos, pos->curr_row, */
+  /*                       pos->curr_col), 0, col, "\u2588"); */
+  /*   } else { */
+  /*     mvwaddstr(get_pos(pos, pos->curr_row, */
+  /*                       pos->curr_col), 0, col, "\u2584"); */
+  /*   } */
+
+  /*   if (p_states.down.is_wall == true) { */
+  /*     mvwaddstr(get_pos(pos, pos->curr_row, */
+  /*                       pos->curr_col), 2, col, "\u2588"); */
+  /*   } else { */
+  /*     mvwaddstr(get_pos(pos, pos->curr_row, */
+  /*                       pos->curr_col), 2, col, "\u2580"); */
+  /*   } */
+  /* } */
+
+  /* if (p_states.left.is_wall == true) { */
+  /*   mvwaddstr(get_pos(pos, pos->curr_row, */
+  /*                     pos->curr_col), 1, 0, "\u2588"); */
+  /* } else { */
+  /*   mvwaddstr(get_pos(pos, pos->curr_row, */
+  /*                     pos->curr_col), 1, 0, "\u2590"); */
+  /* } */
+
+  /* if (p_states.right.is_wall == true) { */
+  /*   mvwaddstr(get_pos(pos, pos->curr_row, */
+  /*                     pos->curr_col), 1, 4, "\u2588"); */
+  /* } else { */
+  /*   mvwaddstr(get_pos(pos, pos->curr_row, */
+  /*                     pos->curr_col), 1, 4, "\u258C"); */
+  /* } */
+
+  /* for (int col = 1; col < GRID_WIDTH - 1; col++) { */
+  /*   mvwaddstr(get_pos(pos, pos->curr_row, */
+  /*                     pos->curr_col), 1, col, "\u2588"); */
+  /* } */
+
+
+  /* move_cur(pos, pos->curr_row, pos->curr_col); */
+  /* wrefresh(get_pos(pos, pos->curr_row, pos->curr_col)); */
